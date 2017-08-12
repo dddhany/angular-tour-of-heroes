af2cc043616798e9c04cedc6d1420e2d879b20ae
Dhany <dddhany@gmail.com>
2017-08-12 21:37:20 +0800

    services with async promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent }  from './hero-detail.component';
+import { AppComponent } from "./app.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+
+import { HeroService } from "./hero.service";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroDetailComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
-export class AppModule { }
+export class AppModule {}
